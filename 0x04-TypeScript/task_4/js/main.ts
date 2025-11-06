7a7026c8950fb858f7c26bf65d0cf0f8cf7aee02
D0nG4667 <D0nG4667@users.noreply.github.com>
2025-11-06 06:25:48 +0300

    fix: main.ts task 4

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -5,12 +5,12 @@
 /// <reference path="./subjects/React.ts" />
 
 // Instantiate and export subject constants
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+const cpp = new Subjects.Cpp();
+const java = new Subjects.Java();
+const react = new Subjects.React();
 
 // Create and export a teacher with experienceTeachingC = 10
-export const cTeacher: Subjects.Teacher = {
+const cTeacher: Subjects.Teacher = {
     firstName: 'John',
     lastName: 'Doe',
     experienceTeachingC: 10,
